418bd72c27e7f18d66c5fc9d830eecfb5d299e3a
igor_yazovcev <uSAg9wbxUX6NEROI>
2020-03-05 12:11:45 +0500

    Updated hello message

--- src/main/kotlin/TestSpark/Hello.kt
+++ src/main/kotlin/TestSpark/Hello.kt
@@ -4,6 +4,7 @@ import spark.Spark.*
 
 fun main(args: Array<String>) {
     println("Hello, World")
+    println("show in web browser \"http://localhost:4567/hello\"")
     
     exception(Exception::class.java) { e, req, res -> e.printStackTrace() }
 
